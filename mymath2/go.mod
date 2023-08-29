d3589c02908cbeda2819a7711342664972d894bb
My Name <myEmail@example.com>
2023-08-29 21:48:19 +0800

    first

---
+++ mymath2/go.mod
@@ -0,0 +1,3 @@
+module github.com/MikeBazh/test/tree/main/mymath
+
+go 1.20
---
+++ mymath2/mymath.go
@@ -0,0 +1,26 @@
+//Необходимо создать внешний пакет с именем mymath,
+//который будет содержать функции для выполнения математических операций.
+//
+//Внешний пакет:
+package mymath
+// Пример кода для иллюстрации задачи
+import "math"
+
+func Sqrt(x float64) float64 {
+return math.Sqrt(x)
+}
+func Floor(x float64) float64 {
+return math.Floor(x)
+}
+func Max(x, y float64) float64 {
+return math.Max(x, y)
+}
+func Min(x, y float64) float64 {
+return math.Min(x, y)
+}
+func Pow(x, y float64) float64 {
+return math.Pow(x, y)
+}
+func Ceil(x float64) float64 {
+return math.Ceil(x)
+}
